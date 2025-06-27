0a244215b2b9423ce03132c7b43e8b585f89cdce
Bahwani <111084575+Bahwani@users.noreply.github.com>
2025-06-28 05:41:32 +0700

    Update new.lua

--- new.lua
+++ new.lua
@@ -109,18 +109,16 @@ local ProximityPromptService = game:GetService("ProximityPromptService")
 
 ProximityPromptService.PromptShown:Connect(function(prompt)
 	if instantEnabled then
-		prompt.HoldDuration = 0
-
-		-- Hindari mengganggu tombol UI
-		local mousePos = UserInputService:GetMouseLocation()
-		if not (mousePos.X >= gui.AbsolutePosition.X and mousePos.X <= gui.AbsolutePosition.X + gui.AbsoluteSize.X and
-		        mousePos.Y >= gui.AbsolutePosition.Y and mousePos.Y <= gui.AbsolutePosition.Y + gui.AbsoluteSize.Y) then
-			prompt.ClickablePrompt = true
-		else
-			prompt.ClickablePrompt = false
-		end
+		task.spawn(function()
+			wait(0.1)
+			if prompt and prompt:IsDescendantOf(workspace) and prompt.Enabled then
+				prompt.HoldDuration = 0
+				prompt.ClickablePrompt = true
+			end
+		end)
 	end
 end)
+
 
 -- Tombol toggle Instant Interaction
 local instantInteractButton = Instance.new("TextButton")
