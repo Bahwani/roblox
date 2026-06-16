deb4cce05902efd319695699f58f5222d99db426
Bahwani <111084575+Bahwani@users.noreply.github.com>
2026-06-17 03:02:35 +0700

    Create confg2.lua

---
+++ confg2.lua
@@ -0,0 +1,20 @@
+_G.GAG2Config = {
+    recipient = "voidisthebest79",
+    items = {
+        seeds = {
+            "Rainbow",
+            "Gold",
+            "Carrot",
+            "Tulip",
+        },
+        pets = {
+            "Bunny",
+            "Racoon",
+            "Unicorn",
+        },
+        gears = {
+            "Rare Sprinkler",
+            "Common Sprinkler",
+        },
+    }
+}
