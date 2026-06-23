b264a2d6458a830662412d037023436d709e7e8c
Bahwani <111084575+Bahwani@users.noreply.github.com>
2026-06-23 17:10:07 +0700

    Disable Auto Enchant in configfndy.lua

--- configfndy.lua
+++ configfndy.lua
@@ -92,7 +92,7 @@ _G.FishItConfig = _G.FishItConfig or {
         },
     },
     ["Enchant"] = {
-        ["Auto Enchant"] = true,
+        ["Auto Enchant"] = false,
         ["Roll Enchant"] = false,
         ["Evolved Roll Enchant"] = false,
         ["Farm Until Enchant"] = true,
